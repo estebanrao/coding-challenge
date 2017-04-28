bf8a65a8c3fbd9b4e30fbe523685eece4898ab70
Esteban Rao <estebanrao@gmail.com>
2017-04-27 23:27:55 -0300

    4th commit

--- js/app.js
+++ js/app.js
@@ -1,51 +1,97 @@
+'use strict';
+
+/* global console */
+
 import '../css/app.scss';
 import $ from "jquery";
 
-let websocket;
-let weather;
-const $document = $(document);
-const $responseTime = $document.find('.response-time');
-const $cityName = $document.find('.city-name');
-const $highTemp = $document.find('.high-temp');
-const $lowTemp = $document.find('.low-temp');
+$(() => {
 
-function init() {
-  // Open WebSockets connection
-  websocket = new WebSocket('ws://echo.websocket.org/');
+  let websocket;
+  const $document = $(document);
+  const $citySearchForm = $document.find('.city-search');
+  const $citySearchFormInput = $document.find('.city-search__input');
+  const $dataContainer = $document.find('.data-container');
+  const $responseTime = $document.find('.response-time');
+  const $cityName = $document.find('.city-name');
+  const $highTemp = $document.find('.high-temp');
+  const $lowTemp = $document.find('.low-temp');
 
-  websocket.onopen = () => {
-    console.info('WebSocket connected');
-    // Once opened use HTML5 GeoLocation to get current location
-    navigator.geolocation.getCurrentPosition(getWeather);
-  };
-  websocket.onclose = () => {
-    console.info('WebSocket disconnected');
-  };
-  websocket.onmessage = (e) => {
-    console.info('Message took: ', e.timeStamp, ' milisecconds');
-    console.info('Message was: ', e.data);
-    $responseTime.text(Math.round(e.timeStamp/1000));
-  };
-}
+  let latency;
+  let latencyIn;
+  let latencyOut;
 
-function getWeather(position) {
-  // This function is called the by the form, or when current location is received
-  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=54c55f2df642a52192aa495ccf01198e`)
-    .then(response => response.json())
-    .then(response => {
-      weather = response.main;
-      console.info('Weather: ', weather);
+  function init() {
+    // Hide Data Container and Search Form
+    $dataContainer.hide();
+    $citySearchForm.hide();
 
-      $highTemp.text(weather.temp_max);
-      $lowTemp.text(weather.temp_min);
-      $cityName.text(response.name);
+    // Open WebSockets connection
+    websocket = new WebSocket('ws://echo.websocket.org/');
 
-      webSocketsSendMessage(weather.temp_max);
-    });
-}
+    websocket.onopen = () => {
+      console.info('WebSocket connected');
+      // Once opened use HTML5 GeoLocation to get current location
+      navigator.geolocation.getCurrentPosition(getWeather);
+    };
+    websocket.onclose = () => {
+      console.info('WebSocket disconnected');
+    };
+    websocket.onmessage = (e) => {
+      latencyOut = Date.now();
+      latency = (latencyOut - latencyIn) / 1000;
 
-function webSocketsSendMessage(weather) {
-  websocket.send(weather);
-}
+      console.info('Message took: ', e.timeStamp, ' milisecconds');
+      console.info('Message latency: ', (latencyOut - latencyIn) / 1000);
+      console.info('Message was: ', e.data);
 
-init();
+      $responseTime.text(latency);
+    };
+  }
+
+  function formSubmit(city) {
+    // Hide Data Container
+    $dataContainer.hide();
+
+    // This function is called the by the form
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=54c55f2df642a52192aa495ccf01198e`)
+      .then(response => response.json())
+      .then(response => doWithWeather(response));
+  }
+
+  function getWeather(position) {
+    // Show form
+    $citySearchForm.show();
+
+    // This function is called when current location is received
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=54c55f2df642a52192aa495ccf01198e`)
+      .then(response => response.json())
+      .then(response => doWithWeather(response));
+  }
+
+  function doWithWeather(response) {
+    // Show Data Container
+    $dataContainer.show();
+
+    console.info('Weather: ', response.main);
+
+    $highTemp.text(response.main.temp_max);
+    $lowTemp.text(response.main.temp_min);
+    $cityName.text(response.name);
+
+    latencyIn = Date.now();
+    webSocketsSendMessage(response.main.temp_max);
+  }
+
+  function webSocketsSendMessage(weather) {
+    websocket.send(weather);
+  }
+
+  init();
+
+  $citySearchForm.submit((e) => {
+    e.preventDefault();
+    formSubmit($citySearchFormInput.val());
+  });
+
+});
